1b2c957e90a704a8ea9a6a9b472f5f4e8961e0ed
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2024-06-01 15:11:01 +0100

    Added my new app

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 import streamlit as st
 from WhatsAppScript import *
+from st_login_form import login_form
 
-st.set_page_config(page_title="Google Sheet to Whatsapp")
+if 'to_login' not in st.session_state:
+    st.session_state['to_login'] = False
+    
+def verify_login(username: str, password):
+    match [username, password]:
+        case [st.secrets.username, st.secrets.password]:
+            st.session_state['to_login'] = True
+            return st.balloons()
+        case _:
+            return st.error("Username or password incorrect")
 
-with st.form("Send data to Whatsapp"):
-    google_sheet_link = st.text_input("Your google sheet Link")
-    if st.form_submit_button("Send"):
-        with st.spinner():
-            script.main(google_sheet_link)
-        st.success("DONE!")
+def main():
+    st.set_page_config(page_title="Google Sheet to Whatsapp")
+    with st.form("Send data to Whatsapp"):
+        google_sheet_link = st.text_input("Your google sheet Link")
+        if st.form_submit_button("Send"):
+            with st.spinner():
+                script.main(google_sheet_link)
+            st.success("DONE!")
+
+if st.session_state['to_login']:
+    main()
+else:
+    with st.form("Login"):
+        username = st.text_input("Username")
+        password = st.text_input("Password")
+        if st.form_submit_button("Login"):
+            verify_login(username, password)
